f36412c8a3b898c7952ac77a81cbade5ab8122cd
greatroott <greatroott>
2022-04-28 11:53:10 +0900

    test

--- train.py
+++ train.py
@@ -1,16 +1,19 @@
 import pandas as pd 
 from autogluon.tabular import TabularPredictor
+from sklearn.model_selection import train_test_split
 import json
 import os  
 
 save_path = "./models"
 data_path = os.path.join("data")
-train = pd.read_csv(os.path.join(data_path,'train.csv'))
+df = pd.read_csv(os.path.join(data_path,'train.csv'))
 
+train,test = train_test_split(df, test_size=0.2, shuffle=True, random_state=34)
 predictor = TabularPredictor(label = "Survived", path = save_path).fit(train_data = train,presets='best_quality', time_limit=40)
-train_acc = predictor.evaluate(train)
+acc = predictor.evaluate(test)
 
 # print to file 
 with open("metrics.json","w") as outfile:
-    json.dump(train_acc,outfile)
+    json.dump(acc,outfile)
+
 
