2cfa5d046b36db6e4e5c0782433f9b11ca01bec5
greatroott <greatroott>
2022-04-28 11:05:49 +0900

    time-limit 40

--- train.py
+++ train.py
@@ -7,7 +7,7 @@ save_path = "./models"
 data_path = os.path.join("data")
 train = pd.read_csv(os.path.join(data_path,'train.csv'))
 
-predictor = TabularPredictor(label = "Target", path = save_path).fit(train_data = train,presets='best_quality', time_limit=30)
+predictor = TabularPredictor(label = "Target", path = save_path).fit(train_data = train,presets='best_quality', time_limit=40)
 train_acc = predictor.evaluate(train)
 
 # print to file 
